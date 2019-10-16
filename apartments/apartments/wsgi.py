7c392c9c21efc3deefd1c8365b5bdc18c9f92ddc
romanyakovlev <yakovlevroman8@gmail.com>
2019-10-16 23:37:29 +0700

    start django & add app

--- apartments/apartments/wsgi.py
+++ apartments/apartments/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for apartments_api project.
+WSGI config for apartments project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apartments_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apartments.settings')
 
 application = get_wsgi_application()
---
+++ apartments/apartments_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApartmentsApiConfig(AppConfig):
+    name = 'apartments_api'
