d030bfb1dbb22ce827976cf8b23a4c39fb3904d7
romanyakovlev <yakovlevroman8@gmail.com>
2019-10-17 00:02:26 +0700

    add apartment model

--- apartments/apartments_api/models.py
+++ apartments/apartments_api/models.py
@@ -1,3 +1,11 @@
+
 from django.db import models
 
-# Create your models here.
+class ApartmentModel(models.Model):
+
+    apartment_price = models.PositiveIntegerField(default=0)
+    number_of_rooms = models.PositiveIntegerField(default=1)
+    apartment_floor = models.PositiveIntegerField(default=1)
+    year_of_construction = models.PositiveIntegerField(default=2019)
+    apartment_adress = models.CharField(max_length=200, blank=False)
+
