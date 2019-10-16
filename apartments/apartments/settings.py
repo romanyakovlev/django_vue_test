6dd45c3a2321831e257eb06ce817a9d04509a681
romanyakovlev <yakovlevroman8@gmail.com>
2019-10-17 00:12:44 +0700

    add model to admin

--- apartments/apartments/settings.py
+++ apartments/apartments/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apartments_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- apartments/apartments_api/admin.py
+++ apartments/apartments_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ApartmentModel
 
-# Register your models here.
+class ApartmentAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(ApartmentModel, ApartmentAdmin)
