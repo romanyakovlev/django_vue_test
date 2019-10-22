93ff054211841575b76d28b38b7f924cbc2d4671
romanyakovlev <yakovlevroman8@gmail.com>
2019-10-23 03:23:03 +0700

    add front vue

---
+++ front/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <ApartmentsSearch />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ApartmentsSearch from '@/components/ApartmentsSearch.vue';
+
+
+export default {
+  name: 'home',
+  components: {
+    ApartmentsSearch,
+  },
+};
+</script>
